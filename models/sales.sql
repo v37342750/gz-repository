9d045c7f964158a5c8576b7107ba3c3a0a2f6147
v37342750@gmail.com <v37342750@gmail.com>
2023-08-01 20:04:58 +0000

    stg_product

--- models/sales.sql
+++ models/sales.sql
@@ -5,7 +5,7 @@ WITH
   sales AS (SELECT * FROM {{ ref('stg_sales') }} )
 
 
-  ,product AS (SELECT * FROM `gz_raw_data.raw_gz_product`)
+  ,product AS (SELECT * FROM {{ ref('stg_product') }} )
 
 SELECT
   s.date_date
@@ -18,12 +18,12 @@ SELECT
   -- revenue --
   ,s.turnover
   -- cost --
-  ,CAST(p.purchSE_PRICE AS FLOAT64) AS purchase_price
-	,ROUND(s.qty*CAST(p.purchSE_PRICE AS FLOAT64),2) AS purchase_cost
+  ,p.purchase_price
+	,ROUND((s.qty*p.purchase_price),2) AS purchase_cost
 	-- margin --
-    ,{{ margin('s.turnover', 's.qty*CAST(p.purchSE_PRICE AS FLOAT64)') }} AS margin 
+    ,{{ margin('s.turnover', 's.qty*p.purchase_price') }} AS margin 
     -- margin percentage --
-    ,{{ margin_percent('s.turnover', 's.qty*CAST(p.purchSE_PRICE AS FLOAT64)') }} AS margin_percent
+    ,{{ margin_percent('s.turnover', 's.qty*p.purchase_price') }} AS margin_percent
 
 FROM sales s
 INNER JOIN product p ON s.products_id = p.products_id
